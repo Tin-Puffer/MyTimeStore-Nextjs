72400adaf43c4d5470178ee7cf02bc426aa21546
NguyenTin <nguyenchitinhhtdtr2018@gmail.com>
2022-11-25 22:56:03 +0700

    change caroulse to antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import css from "../styles/HomeStyle.module.scss";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
+import { Carousel } from "antd";
+import { CarouselRef } from "antd/es/carousel";
 
 function SampleNextArrow(props: any) {
   const { onClick } = props;
@@ -23,7 +25,7 @@ function SamplePrevArrow(props: any) {
 }
 export default function Home() {
   const [change, setChange] = useState<Number>(0);
-
+  const ref = useRef<CarouselRef>(null);
   const settings = {
     arrows: true,
     infinite: true,
@@ -42,24 +44,37 @@ export default function Home() {
         <ul style={{ margin: "0px" }}> {dots} </ul>
       </div>
     ),
-    beforeChange: (oldIndex: any, newIndex: Number) => {
+    beforeChange: (oldIndex: Number, newIndex: Number) => {
       setChange(newIndex);
     },
   };
   useEffect(() => {
     if (typeof window !== "undefined") {
+      // console.log(change);
       const prevSlideElement = document.querySelector(".slick-active");
-      prevSlideElement?.classList.toggle(css.active);
+      prevSlideElement?.classList.add(css.active);
     }
   }, [change]);
   return (
     <div className={css.container}>
       <div className={css.sliderContainer}>
         <div className={css.sliderContent}>
-          <Slider {...settings}>
+          {/* <Slider {...settings}> */}
+          <Carousel
+            dots={false}
+            autoplay
+            autoplaySpeed={5000}
+            // speed={6000}
+            ref={ref}
+            beforeChange={(from, to) => {
+              setChange(to);
+            }}
+          >
             <div className={css.sliderItem}>
-              <div className={[css.SliderImg, css.item1].join(" ")}></div>
-              <div className={css.contentI1}>
+              <div className={[css.SliderImg, css.item1].join(" ")}>
+                <div className={css.cover}></div>
+              </div>
+              <div className={[css.content, css.c1].join(" ")}>
                 <blockquote className={css.main}>
                   <h6>BỘ SƯU TẬP ĐỒNG HỒ</h6>
                   <h3>Trường tồn với thời gian</h3>
@@ -77,14 +92,45 @@ export default function Home() {
               </div>
             </div>
             <div className={css.sliderItem}>
-              <div className={[css.SliderImg, css.item2].join(" ")}></div>
-              <div className={css.tss}></div>
+              <div className={[css.SliderImg, css.item2].join(" ")}>
+                <div className={css.cover}></div>
+              </div>
+              <div className={[css.content, css.c2].join(" ")}>
+                <blockquote className={css.main}>
+                  <h6>Dành riêng cho doanh nhân</h6>
+                  <h3>Đẳng cấp là mãi mãi</h3>
+                  <span>
+                    Bộ sưu tập các mẫu đồng hồ Rolex cổ điển gồm các kiểu dáng
+                    kết hợp những bí quyết của Rolex và những tiêu chuẩn cao
+                    nhất về sự hoàn hảo
+                  </span>
+                </blockquote>
+              </div>
             </div>
             <div className={css.sliderItem}>
-              <div className={[css.SliderImg, css.item3].join(" ")}></div>
-              <div className={css.tss}></div>
+              <div className={[css.SliderImg, css.item3].join(" ")}>
+                <div className={css.cover}></div>
+              </div>
+              <div className={[css.content, css.c3].join(" ")}>
+                <blockquote className={css.main}>
+                  <h3>Định hình phong cách</h3>
+                  <span>
+                    Khám phá bộ sưu tập Rolex bằng cách chọn mẫu, chất liệu,
+                    vành đồng hồ, mặt số và dây đeo tay yêu thích của bạn
+                  </span>
+                </blockquote>
+              </div>
             </div>
-          </Slider>
+            {/* </Slider> */}
+          </Carousel>
+          <button
+            onClick={() => {
+              // if(ref.current?.next();)
+              ref.current?.next();
+            }}
+          >
+            xxxx
+          </button>
         </div>
       </div>
     </div>
