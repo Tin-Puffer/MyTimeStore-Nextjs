4413ef01f7c3da2cb1a5dace8ecb088cb8b77cb1
TinPuffer <nguyenchitinhhtdtr2018@gmail.com>
2023-03-18 10:18:14 +0700

    text chat bot

--- components/FBChatBot.tsx
+++ components/FBChatBot.tsx
@@ -5,7 +5,7 @@ const MyChatBot = () => {
   return (
     <FacebookProvider appId="123456789">
       <SendToMessenger
-        messengerAppId="m.me/100333009685772"
+        messengerAppId="100333009685772"
         pageId="100333009685772"
       />
     </FacebookProvider>
